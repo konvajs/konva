860025263b0d59e9419df1725a6633273bd03702
Eric Rowell <erowell83@gmail.com>
2012-04-14 21:34:36 -0700

    added about 50 unit tests to better test selector support

--- tests/js/unitTests.js
+++ tests/js/unitTests.js
@@ -1419,6 +1419,197 @@ Test.prototype.tests = {
         test(Math.round(rect.getAbsolutePosition().x) === 41, 'rect absolute position x should be about 41');
         test(Math.round(rect.getAbsolutePosition().y) === 92, 'rect absolute position y should be about 92');
     },
+    'NODE - test get() selector by adding shape, then group, then layer': function(containerId) {
+        var stage = new Kinetic.Stage({
+            container: containerId,
+            width: 578,
+            height: 200,
+            name: 'stageName',
+            id: 'stageId'
+        });
+        var layer = new Kinetic.Layer({
+            name: 'layerName',
+            id: 'layerId'
+        });
+        var group = new Kinetic.Group({
+            name: 'groupName',
+            id: 'groupId'
+        });
+        var rect = new Kinetic.Rect({
+            x: 200,
+            y: 20,
+            width: 100,
+            height: 50,
+            fill: 'red',
+            stroke: 'black',
+            strokeWidth: 4,
+            name: 'rectName',
+            id: 'rectId'
+        });
+
+        group.add(rect);
+        layer.add(group);
+        stage.add(layer);
+
+        test(stage.get('.rectName')[0].attrs.id === 'rectId', 'problem with shape name selector');
+        test(stage.get('#rectId')[0].attrs.id === 'rectId', 'problem with shape id selector');
+        test(layer.get('.rectName')[0].attrs.id === 'rectId', 'problem with shape name selector');
+        test(layer.get('#rectId')[0].attrs.id === 'rectId', 'problem with shape id selector');
+        test(group.get('.rectName')[0].attrs.id === 'rectId', 'problem with shape name selector');
+        test(group.get('#rectId')[0].attrs.id === 'rectId', 'problem with shape id selector');
+
+        test(stage.get('.groupName')[0].attrs.id === 'groupId', 'problem with group name selector');
+        test(stage.get('#groupId')[0].attrs.id === 'groupId', 'problem with group id selector');
+        test(layer.get('.groupName')[0].attrs.id === 'groupId', 'problem with group name selector');
+        test(layer.get('#groupId')[0].attrs.id === 'groupId', 'problem with group id selector');
+
+        test(stage.get('.layerName')[0].attrs.id === 'layerId', 'problem with layer name selector');
+        test(stage.get('#layerId')[0].attrs.id === 'layerId', 'problem with layer id selector');
+    },
+    'NODE - test get() selector by adding group, then shape, then layer': function(containerId) {
+        var stage = new Kinetic.Stage({
+            container: containerId,
+            width: 578,
+            height: 200,
+            name: 'stageName',
+            id: 'stageId'
+        });
+        var layer = new Kinetic.Layer({
+            name: 'layerName',
+            id: 'layerId'
+        });
+        var group = new Kinetic.Group({
+            name: 'groupName',
+            id: 'groupId'
+        });
+        var rect = new Kinetic.Rect({
+            x: 200,
+            y: 20,
+            width: 100,
+            height: 50,
+            fill: 'red',
+            stroke: 'black',
+            strokeWidth: 4,
+            name: 'rectName',
+            id: 'rectId'
+        });
+
+        
+        layer.add(group);
+        group.add(rect);
+        stage.add(layer);
+
+        test(stage.get('.rectName')[0].attrs.id === 'rectId', 'problem with shape name selector');
+        test(stage.get('#rectId')[0].attrs.id === 'rectId', 'problem with shape id selector');
+        test(layer.get('.rectName')[0].attrs.id === 'rectId', 'problem with shape name selector');
+        test(layer.get('#rectId')[0].attrs.id === 'rectId', 'problem with shape id selector');
+        test(group.get('.rectName')[0].attrs.id === 'rectId', 'problem with shape name selector');
+        test(group.get('#rectId')[0].attrs.id === 'rectId', 'problem with shape id selector');
+
+        test(stage.get('.groupName')[0].attrs.id === 'groupId', 'problem with group name selector');
+        test(stage.get('#groupId')[0].attrs.id === 'groupId', 'problem with group id selector');
+        test(layer.get('.groupName')[0].attrs.id === 'groupId', 'problem with group name selector');
+        test(layer.get('#groupId')[0].attrs.id === 'groupId', 'problem with group id selector');
+
+        test(stage.get('.layerName')[0].attrs.id === 'layerId', 'problem with layer name selector');
+        test(stage.get('#layerId')[0].attrs.id === 'layerId', 'problem with layer id selector');
+    },
+    'NODE - test get() selector by adding group, then layer, then shape': function(containerId) {
+        var stage = new Kinetic.Stage({
+            container: containerId,
+            width: 578,
+            height: 200,
+            name: 'stageName',
+            id: 'stageId'
+        });
+        var layer = new Kinetic.Layer({
+            name: 'layerName',
+            id: 'layerId'
+        });
+        var group = new Kinetic.Group({
+            name: 'groupName',
+            id: 'groupId'
+        });
+        var rect = new Kinetic.Rect({
+            x: 200,
+            y: 20,
+            width: 100,
+            height: 50,
+            fill: 'red',
+            stroke: 'black',
+            strokeWidth: 4,
+            name: 'rectName',
+            id: 'rectId'
+        });
+
+        
+        layer.add(group);  
+        stage.add(layer);
+        group.add(rect);
+
+        test(stage.get('.rectName')[0].attrs.id === 'rectId', 'problem with shape name selector');
+        test(stage.get('#rectId')[0].attrs.id === 'rectId', 'problem with shape id selector');
+        test(layer.get('.rectName')[0].attrs.id === 'rectId', 'problem with shape name selector');
+        test(layer.get('#rectId')[0].attrs.id === 'rectId', 'problem with shape id selector');
+        test(group.get('.rectName')[0].attrs.id === 'rectId', 'problem with shape name selector');
+        test(group.get('#rectId')[0].attrs.id === 'rectId', 'problem with shape id selector');
+
+        test(stage.get('.groupName')[0].attrs.id === 'groupId', 'problem with group name selector');
+        test(stage.get('#groupId')[0].attrs.id === 'groupId', 'problem with group id selector');
+        test(layer.get('.groupName')[0].attrs.id === 'groupId', 'problem with group name selector');
+        test(layer.get('#groupId')[0].attrs.id === 'groupId', 'problem with group id selector');
+
+        test(stage.get('.layerName')[0].attrs.id === 'layerId', 'problem with layer name selector');
+        test(stage.get('#layerId')[0].attrs.id === 'layerId', 'problem with layer id selector');
+    },
+    'NODE - test get() selector by adding layer, then group, then shape': function(containerId) {
+        var stage = new Kinetic.Stage({
+            container: containerId,
+            width: 578,
+            height: 200,
+            name: 'stageName',
+            id: 'stageId'
+        });
+        var layer = new Kinetic.Layer({
+            name: 'layerName',
+            id: 'layerId'
+        });
+        var group = new Kinetic.Group({
+            name: 'groupName',
+            id: 'groupId'
+        });
+        var rect = new Kinetic.Rect({
+            x: 200,
+            y: 20,
+            width: 100,
+            height: 50,
+            fill: 'red',
+            stroke: 'black',
+            strokeWidth: 4,
+            name: 'rectName',
+            id: 'rectId'
+        });
+    
+        stage.add(layer);
+        layer.add(group);
+        group.add(rect);
+
+        test(stage.get('.rectName')[0].attrs.id === 'rectId', 'problem with shape name selector');
+        test(stage.get('#rectId')[0].attrs.id === 'rectId', 'problem with shape id selector');
+        test(layer.get('.rectName')[0].attrs.id === 'rectId', 'problem with shape name selector');
+        test(layer.get('#rectId')[0].attrs.id === 'rectId', 'problem with shape id selector');
+        test(group.get('.rectName')[0].attrs.id === 'rectId', 'problem with shape name selector');
+        test(group.get('#rectId')[0].attrs.id === 'rectId', 'problem with shape id selector');
+
+        test(stage.get('.groupName')[0].attrs.id === 'groupId', 'problem with group name selector');
+        test(stage.get('#groupId')[0].attrs.id === 'groupId', 'problem with group id selector');
+        test(layer.get('.groupName')[0].attrs.id === 'groupId', 'problem with group name selector');
+        test(layer.get('#groupId')[0].attrs.id === 'groupId', 'problem with group id selector');
+
+        test(stage.get('.layerName')[0].attrs.id === 'layerId', 'problem with layer name selector');
+        test(stage.get('#layerId')[0].attrs.id === 'layerId', 'problem with layer id selector');
+    },
+    
     'NODE - test drag and drop properties and methods': function(containerId) {
         var stage = new Kinetic.Stage({
             container: containerId,
