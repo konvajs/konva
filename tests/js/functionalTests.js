c1b9d448858b1cebf8c6f49415d3baf300bd6b48
Eric Rowell <erowell83@gmail.com>
2012-06-16 00:02:55 -0700

    setup data url hash for functional tests so that we can compare the visual result before and after a series of simulated events

--- tests/js/functionalTests.js
+++ tests/js/functionalTests.js
@@ -1,5 +1,7 @@
 Test.prototype.tests = {
     'DRAG AND DROP - test dragstart, dragmove, dragend': function(containerId) {
+        var urls = dataUrls['DRAG AND DROP - test dragstart, dragmove, dragend'];
+
         var stage = new Kinetic.Stage({
             container: containerId,
             width: 578,
@@ -39,37 +41,45 @@ Test.prototype.tests = {
         circle.on('dragend', function() {
             dragEnd = true;
         });
-        /*
-         * simulate drag and drop
-         */
-        stage._mousedown({
-            offsetX: 380,
-            offsetY: stage.getHeight() / 2
-        });
 
-        test(!dragStart, 'dragstart event should not have been triggered');
-        test(!dragMove, 'dragmove event should not have been triggered');
-        test(!dragEnd, 'dragend event should not have been triggered');
-
-        setTimeout(function() {
-            stage._mousemove({
-                offsetX: 100,
+        stage.toDataURL(function(startDataUrl) {
+            test(urls[0] === startDataUrl, 'start data url is incorrect');
+            /*
+             * simulate drag and drop
+             */
+            stage._mousedown({
+                offsetX: 380,
                 offsetY: stage.getHeight() / 2
             });
 
-            test(dragStart, 'dragstart event was not triggered');
-            test(dragMove, 'dragmove event was not triggered');
+            test(!dragStart, 'dragstart event should not have been triggered');
+            test(!dragMove, 'dragmove event should not have been triggered');
             test(!dragEnd, 'dragend event should not have been triggered');
-        }, 50);
-        setTimeout(function() {
-            stage._mouseup({
-                offsetX: 100,
-                offsetY: stage.getHeight() / 2
-            });
 
-            test(dragStart, 'dragstart event was not triggered');
-            test(dragMove, 'dragmove event was not triggered');
-            test(dragEnd, 'dragend event was not triggered');
-        }, 100);
+            setTimeout(function() {
+                stage._mousemove({
+                    offsetX: 100,
+                    offsetY: stage.getHeight() / 2
+                });
+
+                test(dragStart, 'dragstart event was not triggered');
+                test(dragMove, 'dragmove event was not triggered');
+                test(!dragEnd, 'dragend event should not have been triggered');
+            }, 50);
+            setTimeout(function() {
+                stage._mouseup({
+                    offsetX: 100,
+                    offsetY: stage.getHeight() / 2
+                });
+
+                test(dragStart, 'dragstart event was not triggered');
+                test(dragMove, 'dragmove event was not triggered');
+                test(dragEnd, 'dragend event was not triggered');
+                
+                stage.toDataURL(function(endDataUrl) {
+                	test(urls[1] === endDataUrl, 'end data url is incorrect');
+                });
+            }, 100);
+        });
     }
 };
