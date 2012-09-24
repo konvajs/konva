16c7cd2708ec70f400242e85b054662ac8b28c9a
David Johansson <david.johansson6@gmail.com>
2012-09-24 22:31:06 +0200

    Re-added Kinetic.Collection to _getNodes and a test confirming its necessity.

--- dist/kinetic-core.js
+++ dist/kinetic-core.js
@@ -2711,7 +2711,7 @@ Kinetic.Container.prototype = {
      * get all shapes inside container
      */
     _getNodes: function(sel) {
-        var arr = [];
+        var arr = new Kinetic.Collection();
         function traverse(cont) {
             var children = cont.getChildren();
             for(var n = 0; n < children.length; n++) {
