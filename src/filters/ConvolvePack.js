e631eff6b447dc8f95b025d446bc84e723f423ad
ippo615 <acippolit@gmail.com>
2013-08-04 14:02:17 -0400

    Cleaned convolve pack, separated filter tests.
    I removed the ability to do a convolution with a 1D matrix because I was
    using all 2D matrices. I also cleaned up some unused variables and filters.
    
    I took the entire group of tests dealing with image filters and put them
    into `filterTests.html` and `filterTests.js`. I removed them from
    `visualTests.js`.

--- src/filters/ConvolvePack.js
+++ src/filters/ConvolvePack.js
@@ -14,104 +14,45 @@
 
         // Determine the size and demsionality of the matrix
         // Note: it should be square and odd (3,5,7,9 etc...)
-        var is2D = (matrix[0].length > 0) || 0,
-            matrixSizeX = matrix.length,
-            matrixSizeY = matrix.length,
+        var matrixSizeX = matrix.length,
+            matrixSizeY = matrix[0].length,
             matrixMidX = Math.floor(matrix.length/2),
-            matrixMidY = Math.floor(matrix.length/2);
-
-        // Make sure we don't try to access pixels outside the image
-        var xMax = Math.floor(imageSizeX - matrixSizeX/2),
-            xMin = Math.floor(matrixSizeX/2),
-            yMax = Math.floor(imageSizeY - matrixSizeY/2),
-            yMin = Math.floor(matrixSizeY/2);
+            matrixMidY = Math.floor(matrix[0].length/2);
 
         // Accumlators and positions for iterating
         var r,g,b,a, x,y, px,py, pos, i,j;
 
         // Handle the 2D matrix
-        if( is2D ){
-            for( y=0; y<imageSizeY; y+=1){
-                for( x=0; x<imageSizeX; x+=1){
-    
-                    // Perform the convolution
-                    r = 0; g = 0; b = 0; a = 0;
-                    for( i=0; i<matrixSizeX; i+=1){
-                        for( j=0; j<matrixSizeY; j+=1){
-                            // tile the image to account for pixels past the
-                            // edge (and make sure they are positive)
-                            px = (x+i-matrixMidX) % imageSizeX;
-                            px = (px>0)?px:-px;
-                            py = (y+i-matrixMidY) % imageSizeY;
-                            py = (py>0)?py:-py;
-                            //pos = ((y+j)*imageSizeX+x+i)*4;
-                            // get the pixel
-                            pos = (py*imageSizeX + px)*4;
-                            r += matrix[j][i]*pixels[pos+0];
-                            g += matrix[j][i]*pixels[pos+1];
-                            b += matrix[j][i]*pixels[pos+2];
-                            //a += matrix[j][i]*pixels[pos+3];
-                        }
-                    }
-
-                    // Store the result
-                    pos = (y*imageSizeX+x)*4;
-                    result[pos+0] = r;
-                    result[pos+1] = g;
-                    result[pos+2] = b;
-                    //result[pos+3] = a;
-                }
-            }
-        }
-        // Handle the 1D matrix
-        else{
-            // WARNING: THIS NEEDS TO BE UPDATED!!! TO HANDLE TILING
-            // Horizontal pass (ie convolving every row of the image)
-            for( y=0; y<imageSizeY; y+=1){
-                for( x=xMin; x<xMax; x+=1){
-                    pos = (y*imageSizeX+x)*4;
-    
-                    // Perform the convolution
-                    r = 0; g = 0; b = 0; a = 0;
-                    for( j=0; j<matrixSizeX; j+=1){
-                        pos += 4;
-                        r += matrix[j]*pixels[pos+0];
-                        g += matrix[j]*pixels[pos+1];
-                        b += matrix[j]*pixels[pos+2];
-                        //a += matrix[j]*pixels[pos+3];
-                    }
-
-                    // Multiply by 1/2 because the other 1/2 comes from the vertical pass
-                    pos = (y*imageSizeX+x)*4;
-                    result[pos+0] = r*0.5;
-                    result[pos+1] = g*0.5;
-                    result[pos+2] = b*0.5;
-                    //result[pos+3] = a*0.5;
-                }
-            }
-
-            // Vertical pass (ie convolving every column of the image)
+        for( y=0; y<imageSizeY; y+=1){
             for( x=0; x<imageSizeX; x+=1){
-                for( y=yMin; y<yMax; y+=1){
-                    pos = (y*imageSizeX+x)*4;
-    
-                    // Perform the convolution
-                    r = 0; g = 0; b = 0; a = 0;
-                    for( j=0; j<matrixSizeX; j+=1){
-                        pos += 4*imageSizeX;
-                        r += matrix[j]*pixels[pos+0];
-                        g += matrix[j]*pixels[pos+1];
-                        b += matrix[j]*pixels[pos+2];
-                        //a += matrix[j]*pixels[pos+3];
+
+                // Perform the convolution
+                r = 0; g = 0; b = 0; a = 0;
+                for( i=0; i<matrixSizeX; i+=1){
+                    for( j=0; j<matrixSizeY; j+=1){
+
+                        // tile the image to account for pixels past the
+                        // edge (and make sure they are positive)
+                        px = (x+i-matrixMidX) % imageSizeX;
+                        px = (px>0)?px:-px;
+                        py = (y+i-matrixMidY) % imageSizeY;
+                        py = (py>0)?py:-py;
+
+                        // get the pixel and convolve
+                        pos = (py*imageSizeX + px)*4;
+                        r += matrix[j][i]*pixels[pos+0];
+                        g += matrix[j][i]*pixels[pos+1];
+                        b += matrix[j][i]*pixels[pos+2];
+                        //a += matrix[j][i]*pixels[pos+3];
                     }
-    
-                    // Multiply by 1/2 and add to horizontal pass results
-                    pos = (y*imageSizeX+x)*4;
-                    result[pos+0] += r*0.5;
-                    result[pos+1] += g*0.5;
-                    result[pos+2] += b*0.5;
-                    //result[pos+3] += a*0.5;
                 }
+
+                // Store the result
+                pos = (y*imageSizeX+x)*4;
+                result[pos+0] = r;
+                result[pos+1] = g;
+                result[pos+2] = b;
+                //result[pos+3] = a;
             }
         }
 
@@ -131,7 +72,7 @@
         return Math.pow(Math.E, -dx*dx / (2*sigma*sigma));
     };
 
-    var make_blur_kernel = function( size, scale ){
+    var make_blur_kernel = function( size, scale, sigma ){
 
         // make sure size is odd:
         if( size % 2 === 0 ){ size += 1; }
@@ -142,7 +83,7 @@
         for( i=0; i<size; i+=1 ){
             row = [];
             for( j=0; j<size; j+=1 ){
-                row.push( scale * gaussian(i,size/2,1) * gaussian(j,size/2,1) );
+                row.push( scale * gaussian(i,size/2,sigma) * gaussian(j,size/2,sigma) );
             }
             kernel.push(row);
         }
@@ -150,14 +91,35 @@
         return kernel;
     };
 
+    var make_edge_detect_kernel = function( size, scale, sigma ){
+
+        // make sure size is odd:
+        if( size % 2 === 0 ){ size += 1; }
+
+        // Create a difference-of-gaussians kernel (by subtracting gaussians)
+        // 1.6 is a good sigma ratio to approximate a laplacian of gaussian 
+        var kernel = [], i,j, row, g;
+        for( i=0; i<size; i+=1 ){
+            row = [];
+            for( j=0; j<size; j+=1 ){
+                g1 = gaussian(i,size/2,sigma) * gaussian(j,size/2,sigma);
+                g2 = gaussian(i,size/2,sigma*1.6) * gaussian(j,size/2,sigma*1.6);
+                row.push( scale * (g2-g1) );
+            }
+            kernel.push(row);
+        }
+
+        return kernel;   
+    };
+
     var make_soft_blur_kernel = function( size, percent ){
         // A soft blur is achieve by blurring the image then
         // merging the blured and unblurred image (ie 60/40).
         // Instead of that we've scaling the blur kernel (ie 60)
         // and adding the identity scaled (ie 40) to the kernel
-        var kernel = make_blur_kernel( size, percent / 100 ),
+        var kernel = make_blur_kernel( size, percent, 1 ),
             mid = Math.floor(size/2);
-        kernel[mid][mid] += 1 - (percent/100);
+        kernel[mid][mid] += 1-percent;
         return kernel;
     };
 
@@ -166,9 +128,9 @@
         // and combining it with the original (like a soft blur but
         // with the blur negated). We can achieve this by negating 
         // blur kernel, and adding twice the identity to that kernel.
-        var kernel = make_blur_kernel( size, -percent / 100 ),
+        var kernel = make_blur_kernel( size, -percent, 1 ),
             mid = Math.floor(size/2);
-        kernel[mid][mid] += 1 + (percent/100);
+        kernel[mid][mid] += 1+percent;
         return kernel;
     };
 
@@ -215,10 +177,7 @@
      */
     Kinetic.Filters.UnsharpMask = function(imageData) {
         convolve_internal(imageData,
-            make_unsharp_kernel(
-                5,
-                this.getFilterAmount()
-            )
+            make_unsharp_kernel(5,this.getFilterAmount()/100)
         );
     };
 
@@ -230,26 +189,23 @@
      */
     Kinetic.Filters.SoftBlur = function(imageData) {
         convolve_internal(imageData,
-            make_soft_blur_kernel(
-                5,
-                this.getFilterAmount()
-            )
+            make_soft_blur_kernel(5,this.getFilterAmount()/100)
         );
     };
 
 
     /**
-     * sharpening filter, makes edges more pointed
+     * edge detection filter, makes edges more noticable
      * @function
      * @memberof Kinetic.Filters
      * @param {Object} imageData
      */
-    Kinetic.Filters.Sharpen = function(imageData) {
+    Kinetic.Filters.Edge = function(imageData) {
         var s = this.getFilterAmount()/100;
         if( s === 0 ){ return; }
         convolve_internal(imageData,[
             [ 0,  -1*s, 0],
-            [-1*s, 4*s,-1*s],
+            [-1*s,(1-s)+4*s,-1*s],
             [ 0,  -1*s, 0]
         ]);
     };
@@ -264,81 +220,9 @@
         var s = this.getFilterAmount()/100;
         if( s === 0 ){ return; }
         convolve_internal(imageData,[
-            [-1*s,-.5*s, 0],
-            [-.5*s,1+.5*s, .5*s],
-            [ 0,    .5*s, 1*s]
-        ]);
-    };
-
-    /**
-     * detects horizontal edges 
-     * @function
-     * @memberof Kinetic.Filters
-     * @param {Object} imageData
-     */
-    Kinetic.Filters.DetectHorizontalEdges = function(imageData) {
-        convolve_internal(imageData,[
-            [-1,-1,-1],
-            [ 2, 2, 2],
-            [-1,-1,-1]
-        ]);
-    };
-
-    /**
-     * detects vertical edges 
-     * @function
-     * @memberof Kinetic.Filters
-     * @param {Object} imageData
-     */
-    Kinetic.Filters.DetectVerticalEdges = function(imageData) {
-        convolve_internal(imageData,[
-            [-1, 2,-1],
-            [-1, 2,-1],
-            [-1, 2,-1]
-        ]);
-    };
-
-    /**
-     * detects 45* angle edges 
-     * @function
-     * @memberof Kinetic.Filters
-     * @param {Object} imageData
-     */
-    Kinetic.Filters.DetectDiagonal45Edges = function(imageData) {
-        convolve_internal(imageData,[
-            [-1,-1, 2],
-            [-1, 2,-1],
-            [ 2,-1,-1]
-        ]);
-    };
-
-    /**
-     * detects 135* angle edges 
-     * @function
-     * @memberof Kinetic.Filters
-     * @param {Object} imageData
-     */
-    Kinetic.Filters.DetectDiagonal135Edges = function(imageData) {
-        convolve_internal(imageData,[
-            [ 2,-1,-1],
-            [-1, 2,-1],
-            [-1,-1, 2]
-        ]);
-    };
-
-    /**
-     * detects edges 
-     * @function
-     * @memberof Kinetic.Filters
-     * @param {Object} imageData
-     */
-    Kinetic.Filters.DetectEdges = function(imageData) {
-        var s = this.getFilterAmount()/100;
-        if( s === 0 ){ return; }
-        convolve_internal(imageData,[
-            [-1*s,-1*s,-1*s],
-            [-1*s, 8*s,-1*s],
-            [-1*s,-1*s,-1*s]
+            [-1*s,-0.5*s, 0],
+            [-0.5*s,1+0.5*s, 0.5*s],
+            [ 0,    0.5*s, 1*s]
         ]);
     };
 
