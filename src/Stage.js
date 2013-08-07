3f2e7b88be0322c6cfd48c759fdab44bb638dbae
Elliot Chong <elliotjameschong@gmail.com>
2013-08-06 20:35:44 -0700

    Fixing issue #543 - mouseout should fire before mouseover
    A mouseout event for the current node should be fired before another node fires its mouseover event.

--- src/Stage.js
+++ src/Stage.js
@@ -370,12 +370,12 @@
                 shape = obj.shape;
                 if(shape) {
                     if(!go.isDragging() && obj.pixel[3] === 255 && (!this.targetShape || this.targetShape._id !== shape._id)) {
-                        shape._fireAndBubble(MOUSEOVER, evt, this.targetShape);
-                        shape._fireAndBubble(MOUSEENTER, evt, this.targetShape);
                         if(this.targetShape) {
                             this.targetShape._fireAndBubble(MOUSEOUT, evt, shape);
                             this.targetShape._fireAndBubble(MOUSELEAVE, evt, shape);
                         }
+                        shape._fireAndBubble(MOUSEOVER, evt, this.targetShape);
+                        shape._fireAndBubble(MOUSEENTER, evt, this.targetShape);
                         this.targetShape = shape;
                     }
                     else {
