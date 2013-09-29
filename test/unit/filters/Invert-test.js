98fef5eb674ac7192e15ca2530da72508156d8e9
ippo615 <acippolit@gmail.com>
2013-09-29 14:51:49 -0400

    Added invert filter test

---
+++ test/unit/filters/Invert-test.js
@@ -0,0 +1,84 @@
+suite('Invert', function() {
+    // ======================================================
+    test('basic', function(done) {
+        var stage = addStage();
+
+        var imageObj = new Image();
+        imageObj.onload = function() {
+            
+            var layer = new Kinetic.Layer();
+            darth = new Kinetic.Image({
+                x: 10,
+                y: 10,
+                image: imageObj,
+                draggable: true
+            });
+
+            layer.add(darth);
+            stage.add(layer);
+
+            darth.setFilter(Kinetic.Filters.Invert);
+            layer.draw();
+
+            done();
+        };
+        imageObj.src = 'assets/darth-vader.jpg';
+
+    });
+
+    // ======================================================
+    test('crop', function(done) {
+        var stage = addStage();
+
+        var imageObj = new Image();
+        imageObj.onload = function() {
+            
+            var layer = new Kinetic.Layer();
+            darth = new Kinetic.Image({
+                x: 10,
+                y: 10,
+                image: imageObj,
+                crop: {x:128, y:48, width:256, height:128},
+                draggable: true
+            });
+
+            layer.add(darth);
+            stage.add(layer);
+
+            darth.setFilter(Kinetic.Filters.Invert);
+            layer.draw();
+
+            done();
+
+        };
+        imageObj.src = 'assets/darth-vader.jpg';
+    });
+
+    // ======================================================
+    test('transparancy', function(done) {
+        var stage = addStage();
+
+        var imageObj = new Image();
+        imageObj.onload = function() {
+            
+            var layer = new Kinetic.Layer();
+            darth = new Kinetic.Image({
+                x: 10,
+                y: 10,
+                image: imageObj,
+                draggable: true
+            });
+
+            layer.add(darth);
+            stage.add(layer);
+
+            darth.setFilter(Kinetic.Filters.Invert);
+            layer.draw();
+
+            done();
+        };
+        imageObj.src = 'assets/lion.png';
+
+    });
+
+});
