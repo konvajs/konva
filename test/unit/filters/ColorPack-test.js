26885d18c1613c8591e670c79a63978ea7ab89a0
ippo615 <acippolit@gmail.com>
2013-09-29 14:55:47 -0400

    Added hue shift filter test

--- test/unit/filters/ColorPack-test.js
+++ test/unit/filters/ColorPack-test.js
@@ -89,4 +89,48 @@ suite('Color Pack', function() {
 
     });
 
+
+    // ======================================================
+    test('hue shift tween transparancy', function(done) {
+        var stage = addStage();
+
+        var imageObj = new Image();
+        imageObj.onload = function() {
+            
+            var layer = new Kinetic.Layer();
+            darth = new Kinetic.Image({
+                x: 10,
+                y: 10,
+                image: imageObj,
+                draggable: true
+            });
+
+            layer.add(darth);
+            stage.add(layer);
+
+            darth.setFilter(Kinetic.Filters.ShiftHue);
+            darth.setFilterHueShiftDeg(360);
+            layer.draw();
+
+            var tween = new Kinetic.Tween({
+              node: darth, 
+              duration: 5.0,
+              filterHueShiftDeg: 0,
+              easing: Kinetic.Easings.EaseInOut
+            });
+        
+            darth.on('mouseover', function() {
+              tween.play();
+            });
+      
+            darth.on('mouseout', function() {
+              tween.reverse();
+            });
+
+            done();
+        };
+        imageObj.src = 'assets/lion.png';
+
+    });
+
 });
