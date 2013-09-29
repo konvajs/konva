e98d31e18b558360a91fc40ce7b1a2690ce44608
ippo615 <acippolit@gmail.com>
2013-09-29 15:00:42 -0400

    Added mask filter test

---
+++ test/unit/filters/Mask-test.js
@@ -0,0 +1,42 @@
+suite('Mask', function() {
+
+    // ======================================================
+    test('basic', function(done) {
+        var stage = addStage();
+
+        var imageObj = new Image();
+        imageObj.onload = function() {
+
+            var layer = new Kinetic.Layer({
+                throttle: 999
+            });
+            var bamoon = new Kinetic.Image({
+                x: 0,
+                y: 0,
+                image: imageObj,
+                draggable: true
+            }),
+            filtered = new Kinetic.Image({
+                x: 300,
+                y: 0,
+                image: imageObj,
+                draggable: true
+            });
+
+            layer.add(bamoon);
+            layer.add(filtered);
+            stage.add(layer);
+
+            filtered.setFilter(Kinetic.Filters.Mask);
+            filtered.setFilterThreshold(10);
+
+            layer.draw();
+
+            done();
+
+        };
+        imageObj.src = 'assets/bamoon.jpg';
+
+    });
+
+});
