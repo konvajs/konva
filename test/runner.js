1965f61da277ed30df5c8410e73f9b02a99c59f2
Eric Rowell <erowell83@gmail.com>
2013-09-30 07:52:13 -0700

    added assertion counter to Mocha

--- test/runner.js
+++ test/runner.js
@@ -1,6 +1,47 @@
 mocha.ui('tdd');
 var assert = chai.assert,
-    kineticContainer = document.getElementById('kinetic-container');
+    kineticContainer = document.getElementById('kinetic-container'),
+    origAssertEqual = assert.equal,
+    origAssert = assert,
+    origNotEqual = assert.notEqual,
+    assertionCount = 0,
+    assertions = document.createElement('em');
+
+function init() {
+  // assert extenders so that we can count assertions
+  assert = function() {
+    origAssert.apply(this, arguments);
+    assertions.innerHTML = ++assertionCount;
+  };
+  assert.equal = function() {
+    origAssertEqual.apply(this, arguments);
+    assertions.innerHTML = ++assertionCount;
+  };
+  assert.notEqual = function() {
+    origNotEqual.apply(this, arguments);
+    assertions.innerHTML = ++assertionCount;
+  };
+
+  window.onload = function() {
+    var mochaStats = document.getElementById('mocha-stats');
+
+    if (mochaStats) {
+      var li = document.createElement('li');
+      var anchor = document.createElement('a');
+
+      anchor.href = '#';
+      anchor.innerHTML = 'assertions:';
+      assertions.innerHTML = 0;
+
+      li.appendChild(anchor);
+      li.appendChild(assertions);
+      mochaStats.appendChild(li);
+    }
+  }
+}
+
+
+
 
 Kinetic.enableTrace = true;
 // make sure pixel ratio is 1 or else the tests will fail on devices with retina display
@@ -47,3 +88,5 @@ beforeEach(function(){
     Kinetic.DD.isDragging = false;
     Kinetic.DD.node = undefined;
 });
+
+init();
