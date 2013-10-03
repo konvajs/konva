a246cb0de193562b880329f390466bd9b8802920
kzhdev <kun.zhao@tradingtechnologies.com>
2013-10-03 15:00:15 -0500

    Fixed an issue where enableShadow/disableShadow didn't work after shap added into a layer

--- src/Shape.js
+++ src/Shape.js
@@ -45,7 +45,7 @@
 
             this._setDrawFuncs();
 
-            this.on('shadowColorChange.kinetic shadowBlurChange.kinetic shadowOffsetChange.kinetic shadowOpacityChange.kinetic', _clearHasShadowCache);
+            this.on('shadowColorChange.kinetic shadowBlurChange.kinetic shadowOffsetChange.kinetic shadowOpacityChange.kinetic shadowEnabledChanged', _clearHasShadowCache);
         },
         hasChildren: function() {
             return false;
