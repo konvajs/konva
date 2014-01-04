b52845b32bca885daf99c1ff0127ab70eb18d93f
Eric Rowell <erowell83@gmail.com>
2014-01-04 06:37:08 -0800

    added another hsv test

--- test/unit/filters/HSV-test.js
+++ test/unit/filters/HSV-test.js
@@ -46,6 +46,51 @@ suite('HSV', function() {
     });
 
     // ======================================================
+    test('saturate image', function(done) {
+        var stage = addStage();
+
+        var imageObj = new Image();
+        imageObj.onload = function() {
+            
+            var layer = new Kinetic.Layer();
+            darth = new Kinetic.Image({
+                x: 10,
+                y: 10,
+                image: imageObj,
+                draggable: true
+            });
+
+            layer.add(darth);
+            stage.add(layer);
+
+            darth.cache();
+            darth.filters([Kinetic.Filters.HSV]);
+
+            darth.saturation(2);
+            layer.draw();
+
+            var tween = new Kinetic.Tween({
+              node: darth, 
+              duration: 1.0,
+              saturation: 0,
+              easing: Kinetic.Easings.EaseInOut
+            });
+        
+            darth.on('mouseover', function() {
+              tween.play();
+            });
+      
+            darth.on('mouseout', function() {
+              tween.reverse();
+            });
+
+            done();
+        };
+        imageObj.src = 'assets/bamoon.jpg';
+
+    });
+
+    // ======================================================
     test('saturation tween transparancy', function(done) {
         var stage = addStage();
 
