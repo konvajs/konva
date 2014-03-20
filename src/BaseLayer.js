a9b63fc8ed649ac939b71c698a7f6d7a2ca7dfce
Eric Rowell <erowell83@gmail.com>
2014-03-20 11:15:55 -0700

    added new BaseLayer, which Layer and FastLayer extend

---
+++ src/BaseLayer.js
@@ -0,0 +1,158 @@
+(function() {
+    Kinetic.Util.addMethods(Kinetic.BaseLayer, {
+        ___init: function(config) {
+            this.nodeType = 'Layer';
+            Kinetic.Container.call(this, config);
+        },
+        createPNGStream : function() {
+            return this.canvas._canvas.createPNGStream();
+        },
+        /**
+         * get layer canvas
+         * @method
+         * @memberof Kinetic.BaseLayer.prototype
+         */
+        getCanvas: function() {
+            return this.canvas;
+        },
+        /**
+         * get layer hit canvas
+         * @method
+         * @memberof Kinetic.BaseLayer.prototype
+         */
+        getHitCanvas: function() {
+            return this.hitCanvas;
+        },
+        /**
+         * get layer canvas context
+         * @method
+         * @memberof Kinetic.BaseLayer.prototype
+         */
+        getContext: function() {
+            return this.getCanvas().getContext();
+        },
+        /**
+         * clear scene and hit canvas contexts tied to the layer
+         * @method
+         * @memberof Kinetic.BaseLayer.prototype
+         * @param {Object} [bounds]
+         * @param {Number} [bounds.x]
+         * @param {Number} [bounds.y]
+         * @param {Number} [bounds.width]
+         * @param {Number} [bounds.height]
+         * @example
+         * layer.clear();<br>
+         * layer.clear(0, 0, 100, 100);
+         */
+        clear: function(bounds) {
+            this.getContext().clear(bounds);
+            this.getHitCanvas().getContext().clear(bounds);
+            return this;
+        },
+        // extend Node.prototype.setZIndex
+        setZIndex: function(index) {
+            Kinetic.Node.prototype.setZIndex.call(this, index);
+            var stage = this.getStage();
+            if(stage) {
+                stage.content.removeChild(this.getCanvas()._canvas);
+
+                if(index < stage.getChildren().length - 1) {
+                    stage.content.insertBefore(this.getCanvas()._canvas, stage.getChildren()[index + 1].getCanvas()._canvas);
+                }
+                else {
+                    stage.content.appendChild(this.getCanvas()._canvas);
+                }
+            }
+            return this;
+        },
+        // extend Node.prototype.moveToTop
+        moveToTop: function() {
+            Kinetic.Node.prototype.moveToTop.call(this);
+            var stage = this.getStage();
+            if(stage) {
+                stage.content.removeChild(this.getCanvas()._canvas);
+                stage.content.appendChild(this.getCanvas()._canvas);
+            }
+        },
+        // extend Node.prototype.moveUp
+        moveUp: function() {
+            if(Kinetic.Node.prototype.moveUp.call(this)) {
+                var stage = this.getStage();
+                if(stage) {
+                    stage.content.removeChild(this.getCanvas()._canvas);
+
+                    if(this.index < stage.getChildren().length - 1) {
+                        stage.content.insertBefore(this.getCanvas()._canvas, stage.getChildren()[this.index + 1].getCanvas()._canvas);
+                    }
+                    else {
+                        stage.content.appendChild(this.getCanvas()._canvas);
+                    }
+                }
+            }
+        },
+        // extend Node.prototype.moveDown
+        moveDown: function() {
+            if(Kinetic.Node.prototype.moveDown.call(this)) {
+                var stage = this.getStage();
+                if(stage) {
+                    var children = stage.getChildren();
+                    stage.content.removeChild(this.getCanvas()._canvas);
+                    stage.content.insertBefore(this.getCanvas()._canvas, children[this.index + 1].getCanvas()._canvas);
+                }
+            }
+        },
+        // extend Node.prototype.moveToBottom
+        moveToBottom: function() {
+            if(Kinetic.Node.prototype.moveToBottom.call(this)) {
+                var stage = this.getStage();
+                if(stage) {
+                    var children = stage.getChildren();
+                    stage.content.removeChild(this.getCanvas()._canvas);
+                    stage.content.insertBefore(this.getCanvas()._canvas, children[1].getCanvas()._canvas);
+                }
+            }
+        },
+        getLayer: function() {
+            return this;
+        },
+        remove: function() {
+            var stage = this.getStage(),
+                canvas = this.getCanvas(),
+                _canvas = canvas._canvas;
+
+            Kinetic.Node.prototype.remove.call(this);
+
+            if(stage && _canvas && Kinetic.Util._isInDocument(_canvas)) {
+                stage.content.removeChild(_canvas);
+            }
+            return this;
+        },
+        getStage: function() {
+            return this.parent;
+        }
+    });
+    Kinetic.Util.extend(Kinetic.BaseLayer, Kinetic.Container);
+
+    // add getters and setters
+    Kinetic.Factory.addGetterSetter(Kinetic.BaseLayer, 'clearBeforeDraw', true);
+    /**
+     * get/set clearBeforeDraw flag which determines if the layer is cleared or not
+     *  before drawing
+     * @name clearBeforeDraw
+     * @method
+     * @memberof Kinetic.BaseLayer.prototype
+     * @param {Boolean} clearBeforeDraw
+     * @returns {Boolean}
+     * @example
+     * // get clearBeforeDraw flag<br>
+     * var clearBeforeDraw = layer.clearBeforeDraw();<br><br>
+     *
+     * // disable clear before draw<br>
+     * layer.clearBeforeDraw(false);<br><br>
+     *
+     * // enable clear before draw<br>
+     * layer.clearBeforeDraw(true);
+     */
+
+    Kinetic.Collection.mapMethods(Kinetic.BaseLayer);
+})();
--- src/FastLayer.js
+++ src/FastLayer.js
@@ -5,11 +5,11 @@
         DRAW = 'draw';
 
     Kinetic.Util.addMethods(Kinetic.FastLayer, {
-        ___init: function(config) {
+        ____init: function(config) {
             this.nodeType = 'Layer';
             this.canvas = new Kinetic.SceneCanvas();
             // call super constructor
-            Kinetic.Container.call(this, config);
+            Kinetic.BaseLayer.call(this, config);
         },
         _validateAdd: function(child) {
             var type = child.getType();
@@ -25,9 +25,6 @@
         },
         getIntersection: function() {
             return null;
-        },
-        createPNGStream : function() {
-            return this.canvas._canvas.createPNGStream();
         },
         drawScene: function(can) {
             var layer = this.getLayer(),
@@ -52,22 +49,6 @@
             this.drawScene();
             return this;
         },  
-        /**
-         * get layer canvas
-         * @method
-         * @memberof Kinetic.FastLayer.prototype
-         */
-        getCanvas: function() {
-            return this.canvas;
-        },
-        /**
-         * get layer canvas context
-         * @method
-         * @memberof Kinetic.FastLayer.prototype
-         */
-        getContext: function() {
-            return this.getCanvas().getContext();
-        },
         /**
          * clear scene and hit canvas contexts tied to the layer
          * @method
@@ -95,111 +76,9 @@
                 this.getCanvas()._canvas.style.display = 'none';
             }
             return this;
-        },
-        // extend Node.prototype.setZIndex
-        setZIndex: function(index) {
-            Kinetic.Node.prototype.setZIndex.call(this, index);
-            var stage = this.getStage();
-            if(stage) {
-                stage.content.removeChild(this.getCanvas()._canvas);
-
-                if(index < stage.getChildren().length - 1) {
-                    stage.content.insertBefore(this.getCanvas()._canvas, stage.getChildren()[index + 1].getCanvas()._canvas);
-                }
-                else {
-                    stage.content.appendChild(this.getCanvas()._canvas);
-                }
-            }
-            return this;
-        },
-        // extend Node.prototype.moveToTop
-        moveToTop: function() {
-            Kinetic.Node.prototype.moveToTop.call(this);
-            var stage = this.getStage();
-            if(stage) {
-                stage.content.removeChild(this.getCanvas()._canvas);
-                stage.content.appendChild(this.getCanvas()._canvas);
-            }
-        },
-        // extend Node.prototype.moveUp
-        moveUp: function() {
-            if(Kinetic.Node.prototype.moveUp.call(this)) {
-                var stage = this.getStage();
-                if(stage) {
-                    stage.content.removeChild(this.getCanvas()._canvas);
-
-                    if(this.index < stage.getChildren().length - 1) {
-                        stage.content.insertBefore(this.getCanvas()._canvas, stage.getChildren()[this.index + 1].getCanvas()._canvas);
-                    }
-                    else {
-                        stage.content.appendChild(this.getCanvas()._canvas);
-                    }
-                }
-            }
-        },
-        // extend Node.prototype.moveDown
-        moveDown: function() {
-            if(Kinetic.Node.prototype.moveDown.call(this)) {
-                var stage = this.getStage();
-                if(stage) {
-                    var children = stage.getChildren();
-                    stage.content.removeChild(this.getCanvas()._canvas);
-                    stage.content.insertBefore(this.getCanvas()._canvas, children[this.index + 1].getCanvas()._canvas);
-                }
-            }
-        },
-        // extend Node.prototype.moveToBottom
-        moveToBottom: function() {
-            if(Kinetic.Node.prototype.moveToBottom.call(this)) {
-                var stage = this.getStage();
-                if(stage) {
-                    var children = stage.getChildren();
-                    stage.content.removeChild(this.getCanvas()._canvas);
-                    stage.content.insertBefore(this.getCanvas()._canvas, children[1].getCanvas()._canvas);
-                }
-            }
-        },
-        getLayer: function() {
-            return this;
-        },
-        remove: function() {
-            var stage = this.getStage(),
-                canvas = this.getCanvas(),
-                _canvas = canvas._canvas;
-
-            Kinetic.Node.prototype.remove.call(this);
-
-            if(stage && _canvas && Kinetic.Util._isInDocument(_canvas)) {
-                stage.content.removeChild(_canvas);
-            }
-            return this;
-        },
-        getStage: function() {
-            return this.parent;
         }
     });
-    Kinetic.Util.extend(Kinetic.FastLayer, Kinetic.Container);
-
-    // add getters and setters
-    Kinetic.Factory.addGetterSetter(Kinetic.FastLayer, 'clearBeforeDraw', true);
-    /**
-     * get/set clearBeforeDraw flag which determines if the layer is cleared or not
-     *  before drawing
-     * @name clearBeforeDraw
-     * @method
-     * @memberof Kinetic.FastLayer.prototype
-     * @param {Boolean} clearBeforeDraw
-     * @returns {Boolean}
-     * @example
-     * // get clearBeforeDraw flag<br>
-     * var clearBeforeDraw = layer.clearBeforeDraw();<br><br>
-     *
-     * // disable clear before draw<br>
-     * layer.clearBeforeDraw(false);<br><br>
-     *
-     * // enable clear before draw<br>
-     * layer.clearBeforeDraw(true);
-     */
+    Kinetic.Util.extend(Kinetic.FastLayer, Kinetic.BaseLayer);
 
     Kinetic.Collection.mapMethods(Kinetic.FastLayer);
 })();
