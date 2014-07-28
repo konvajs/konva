6fc5019f044d952c07bf0e5a8a48cf4cc32bcedd
Matthew Hasbach <hasbach.git@gmail.com>
2014-07-27 21:37:50 -0400

    Kinetic.Node.moveTo() now checks if the node is already a child of the target container before removing it from the current container and adding it to the target container.

--- src/Node.js
+++ src/Node.js
@@ -1031,8 +1031,10 @@
          * node.moveTo(layer2);
          */
         moveTo: function(newContainer) {
-            Kinetic.Node.prototype.remove.call(this);
-            newContainer.add(this);
+            if (this.getParent() !== newContainer) {
+                this.remove();
+                newContainer.add(this);
+            }
             return this;
         },
         /**
