de60359b48b17d4f78b2970f196a186b1777bc4b
lavrton <lavrton@gmail.com>
2015-04-14 09:45:10 +0700

    unit test for filters + pixelRatio

---
+++ test/unit/filters/Filter-test.js
@@ -0,0 +1,28 @@
+"use strict";
+
+suite.only('Filter', function() {
+
+    test('pixelRaio check', function () {
+        Konva.pixelRatio = 2;
+        var stage = addStage();
+        var layer = new Konva.Layer();
+
+        var circle = new Konva.Circle({
+            x : stage.width() / 2,
+            y : stage.height() / 2,
+            fill : 'red',
+            stroke: 'green',
+            radius : 15
+        });
+
+        layer.add(circle);
+        stage.add(layer);
+        circle.cache();
+        circle.filters([Konva.Filters.Blur ]);
+        circle.blurRadius(0);
+        layer.draw();
+
+        cloneAndCompareLayer(layer, 20);
+        Konva.pixelRatio = 1;
+    });
+});
