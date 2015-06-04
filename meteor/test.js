c10f0f4b0001cbc16f02762d398148793a6654d3
Arturs <arturs.sprogis@gmail.com>
2015-06-04 19:23:41 +0300

    Changed readme

--- meteor/test.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-Tinytest.add('Hammer.is', function (test) {
-  var div = document.createElement('div');
-  var mc = new Hammer(div);
-  test.instanceOf(mc, Hammer.Manager, 'Instantiation OK');
-  test.instanceOf(mc.get('pinch').manager, Hammer.Manager, 'Default pinch recognizer OK');
-});
