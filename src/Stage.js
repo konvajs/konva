5df8d4f5a4e61a64434b231fb288a22edb5cca7c
Manuel Rauber <manuel.rauber@thinktecture.com>
2015-09-15 23:16:39 +0200

    Config to not export invisible layers

--- src/Stage.js
+++ src/Stage.js
@@ -223,6 +223,7 @@
          * @param {Number} [config.quality] jpeg quality.  If using an "image/jpeg" mimeType,
          *  you can specify the quality from 0 to 1, where 0 is very poor quality and 1
          *  is very high quality
+         * @param {Boolean} [config.exportVisibleLayersOnly=false] If set to true, the stage will only export a layer which is visible.
          */
         toDataURL: function(config) {
             config = config || {};
@@ -231,6 +232,7 @@
                 quality = config.quality || null,
                 x = config.x || 0,
                 y = config.y || 0,
+                exportVisibleLayersOnly = !!config.exportVisibleLayersOnly || false,
                 canvas = new Konva.SceneCanvas({
                     width: config.width || this.getWidth(),
                     height: config.height || this.getHeight(),
@@ -245,6 +247,10 @@
 
 
             layers.each(function(layer) {
+                if (exportVisibleLayersOnly && !layer.isVisible()) {
+                    return;
+                }
+
                 var width = layer.getCanvas().getWidth();
                 var height = layer.getCanvas().getHeight();
                 var ratio = layer.getCanvas().getPixelRatio();
