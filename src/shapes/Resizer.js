551a013a7922576a9850dc1c988ec6ecbe16e415
Anton Lavrenov <lavrton@gmail.com>
2017-12-21 09:57:16 +0700

    fix resizer bugs, fix lint errors

--- src/shapes/Resizer.js
+++ src/shapes/Resizer.js
@@ -64,7 +64,7 @@
       anchor.on('mouseout', function() {
         var layer = this.getLayer();
         document.body.style.cursor = 'default';
-        this.setStrokeWidth(2);
+        this.setStrokeWidth(1);
         layer.draw();
       });
       this.add(anchor);
@@ -79,7 +79,6 @@
       this.sin = height / hypotenuse;
       this.cos = width / hypotenuse;
 
-      //     console.log(1, hypotenuse);
       window.addEventListener('mousemove', this.handleMouseMove);
       window.addEventListener('touchmove', this.handleMouseMove);
       window.addEventListener('mouseup', this.handleMouseUp);
@@ -87,6 +86,7 @@
     },
 
     handleMouseMove: function(e) {
+      var x, y, newHypotenuse;
       var resizerNode = this.findOne('.' + this.movingResizer);
       var stage = resizerNode.getStage();
 
@@ -107,29 +107,26 @@
       resizerNode.setAbsolutePosition(newAbsPos);
 
       if (this.movingResizer === 'top-left') {
-        var newHypotenuse = Math.sqrt(
+        newHypotenuse = Math.sqrt(
           Math.pow(this.findOne('.bottom-right').x() - resizerNode.x(), 2) +
             Math.pow(this.findOne('.bottom-right').y() - resizerNode.y(), 2)
         );
 
-        //       console.log(2, newHypotenuse);
-        var x = newHypotenuse * this.cos;
-        var y = newHypotenuse * this.sin;
-
-        //       console.log(this.findOne('.bottom-right').x() - x);
+        x = newHypotenuse * this.cos;
+        y = newHypotenuse * this.sin;
 
         this.findOne('.top-left').x(this.findOne('.bottom-right').x() - x);
         this.findOne('.top-left').y(this.findOne('.bottom-right').y() - y);
       } else if (this.movingResizer === 'top-center') {
         this.findOne('.top-left').y(resizerNode.y());
       } else if (this.movingResizer === 'top-right') {
-        var newHypotenuse = Math.sqrt(
+        newHypotenuse = Math.sqrt(
           Math.pow(this.findOne('.bottom-left').x() - resizerNode.x(), 2) +
             Math.pow(this.findOne('.bottom-left').y() - resizerNode.y(), 2)
         );
 
-        var x = newHypotenuse * this.cos;
-        var y = newHypotenuse * this.sin;
+        x = newHypotenuse * this.cos;
+        y = newHypotenuse * this.sin;
 
         this.findOne('.top-right').x(x);
         this.findOne('.top-right').y(this.findOne('.bottom-left').y() - y);
@@ -142,49 +139,43 @@
       } else if (this.movingResizer === 'middle-right') {
         this.findOne('.bottom-right').x(resizerNode.x());
       } else if (this.movingResizer === 'bottom-left') {
-        var newHypotenuse = Math.sqrt(
+        newHypotenuse = Math.sqrt(
           Math.pow(this.findOne('.top-right').x() - resizerNode.x(), 2) +
             Math.pow(this.findOne('.top-right').y() - resizerNode.y(), 2)
         );
 
-        var x = newHypotenuse * this.cos;
-        var y = newHypotenuse * this.sin;
+        x = newHypotenuse * this.cos;
+        y = newHypotenuse * this.sin;
 
         this.findOne('.bottom-left').x(this.findOne('.top-right').x() - x);
         this.findOne('.bottom-left').y(y);
 
-        var pos = resizerNode.position();
+        pos = resizerNode.position();
 
         this.findOne('.top-left').x(pos.x);
         this.findOne('.bottom-right').y(pos.y);
       } else if (this.movingResizer === 'bottom-center') {
         this.findOne('.bottom-right').y(resizerNode.y());
       } else if (this.movingResizer === 'bottom-right') {
-        var newHypotenuse = Math.sqrt(
+        newHypotenuse = Math.sqrt(
           Math.pow(this.findOne('.bottom-right').x(), 2) +
             Math.pow(this.findOne('.bottom-right').y(), 2)
         );
 
-        var x = newHypotenuse * this.cos;
-        var y = newHypotenuse * this.sin;
+        x = newHypotenuse * this.cos;
+        y = newHypotenuse * this.sin;
 
         this.findOne('.bottom-right').x(x);
         this.findOne('.bottom-right').y(y);
       } else if (this.movingResizer === 'rotater') {
-        var x = resizerNode.x() - this._el.width() * this._el.scaleX() / 2;
-        var y = -resizerNode.y() + this._el.height() * this._el.scaleY() / 2;
+        x = resizerNode.x() - this._el.width() * this._el.scaleX() / 2;
+        y = -resizerNode.y() + this._el.height() * this._el.scaleY() / 2;
 
         var dAlpha = Math.atan2(-y, x) + Math.PI / 2;
         var attrs = this._getAttrs();
 
         var newRotation = this.rotation() + dAlpha / Math.PI * 180;
-        // console.log(newRotation);
 
-        // if (this._el._centroid) {
-        //   this._setElementAttrs({
-        //     rotation: newRotation
-        //   });
-        // }
         var alpha = Konva.Util._degToRad(this._el.rotation());
         var newAlpha = Konva.Util._degToRad(newRotation);
 
@@ -214,27 +205,22 @@
         return;
       }
 
-      var layer = resizerNode.getLayer();
       var absPos = this.findOne('.top-left').getAbsolutePosition();
 
-      var x = Math.round(absPos.x);
-      var y = Math.round(absPos.y);
-      var width = Math.round(
-        this.findOne('.bottom-right').x() - this.findOne('.top-left').x()
-      );
+      x = absPos.x;
+      y = absPos.y;
+      var width =
+        this.findOne('.bottom-right').x() - this.findOne('.top-left').x();
 
-      var height = Math.round(
-        this.findOne('.bottom-right').y() - this.findOne('.top-left').y()
-      );
+      var height =
+        this.findOne('.bottom-right').y() - this.findOne('.top-left').y();
 
-      var pos = {
-        x: this._el._centroid ? x + width / 2 : x,
-        y: this._el._centroid ? y + height / 2 : y,
+      this._setElementAttrs({
+        absX: this._el._centroid ? x + width / 2 : x,
+        absY: this._el._centroid ? y + height / 2 : y,
         width: width,
         height: height
-      };
-
-      this._setElementAttrs(pos);
+      });
     },
 
     handleMouseUp: function() {
@@ -248,7 +234,6 @@
       if (this._el._centroid) {
         var topLeftResizer = this.findOne('.top-left');
         var absPos = topLeftResizer.getAbsolutePosition();
-        console.log(absPos);
 
         return {
           x: absPos.x,
@@ -277,10 +262,13 @@
         var scaleY = attrs.height / this._el.height();
 
         this._el.setAttrs({
-          x: attrs.x,
-          y: attrs.y,
           scaleX: scaleX,
           scaleY: scaleY
+        });
+
+        this._el.setAbsolutePosition({
+          x: attrs.absX,
+          y: attrs.absY
         });
       }
       this._update();
@@ -302,40 +290,40 @@
         });
       }
 
-      this.get('.top-left')[0].position({
+      this.findOne('.top-left').position({
         x: 0,
         y: 0
       });
-      this.get('.top-center')[0].position({
+      this.findOne('.top-center').position({
         x: width / 2,
         y: 0
       });
-      this.get('.top-right')[0].position({
+      this.findOne('.top-right').position({
         x: width,
         y: 0
       });
-      this.get('.middle-left')[0].position({
+      this.findOne('.middle-left').position({
         x: 0,
         y: height / 2
       });
-      this.get('.middle-right')[0].position({
+      this.findOne('.middle-right').position({
         x: width,
         y: height / 2
       });
-      this.get('.bottom-left')[0].position({
+      this.findOne('.bottom-left').position({
         x: 0,
         y: height
       });
-      this.get('.bottom-center')[0].position({
+      this.findOne('.bottom-center').position({
         x: width / 2,
         y: height
       });
-      this.get('.bottom-right')[0].position({
+      this.findOne('.bottom-right').position({
         x: width,
         y: height
       });
 
-      this.get('.rotater')[0].position({
+      this.findOne('.rotater').position({
         x: width / 2,
         y: -50
       });
