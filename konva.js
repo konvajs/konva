881da4d1f2c30b3fc496514c89e4e44dd3fa0990
Anton Lavrenov <lavrton@gmail.com>
2018-03-16 08:56:13 +0700

    build for 2.0.2

--- konva.js
+++ konva.js
@@ -1,5 +1,5 @@
 /*
- * Konva JavaScript Framework v2.0.1
+ * Konva JavaScript Framework v2.0.2
  * http://konvajs.github.io/
  * Licensed under the MIT
  * Date: Fri Mar 16 2018
@@ -21,7 +21,7 @@
 
   var Konva = {
     // public
-    version: '2.0.1',
+    version: '2.0.2',
 
     // private
     stages: [],
