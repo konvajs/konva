5998d38878d600f57ba5df08f5b381538bf335e4
George Gkasdrogkas <georgegkas@gmail.com>
2019-07-28 21:29:01 +0300

    feat(ColorToRGBA): support HSL format
    closes issue #505

--- test/unit/Util-test.js
+++ test/unit/Util-test.js
@@ -25,4 +25,27 @@ suite('Util', function() {
       }
     });
   });
+
+  test('colorToRGBA() - from HSL to RGBA conversion', function() {
+    assert.deepEqual(Konva.Util.colorToRGBA('hsl(0, 0%, 0%)'), {
+      r: 0,
+      g: 0,
+      b: 0,
+      a: 1
+    });
+
+    assert.deepEqual(Konva.Util.colorToRGBA('hsl(96, 48%, 59%)'), {
+      r: 140,
+      g: 201,
+      b: 100,
+      a: 1
+    });
+
+    assert.deepEqual(Konva.Util.colorToRGBA('hsl(200, 100%, 70%)'), {
+      r: 102,
+      g: 204,
+      b: 255,
+      a: 1
+    });
+  });
 });
