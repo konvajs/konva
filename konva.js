6904390b3f018f4e6a1d24e011c9ab1ac74b8935
Anton Lavrenov <lavrton@gmail.com>
2019-08-31 16:40:59 -0500

    build for 4.0.6

--- konva.js
+++ konva.js
@@ -5,7 +5,7 @@
 }(this, function () { 'use strict';
 
   /*
-   * Konva JavaScript Framework v4.0.5
+   * Konva JavaScript Framework v4.0.6
    * http://konvajs.org/
    * Licensed under the MIT
    * Date: Sat Aug 31 2019
@@ -76,7 +76,7 @@
               : {};
   var Konva = {
       _global: glob,
-      version: '4.0.5',
+      version: '4.0.6',
       isBrowser: detectBrowser(),
       isUnminified: /param/.test(function (param) { }.toString()),
       dblClickWindow: 400,
