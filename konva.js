a3966169d0485f7472487200be1616377bac3cff
Anton Lavrenov <lavrton@gmail.com>
2020-07-30 10:55:56 -0500

    build for 7.0.4

--- konva.js
+++ konva.js
@@ -5,7 +5,7 @@
 }(this, (function () { 'use strict';
 
   /*
-   * Konva JavaScript Framework v7.0.3
+   * Konva JavaScript Framework v7.0.4
    * http://konvajs.org/
    * Licensed under the MIT
    * Date: Thu Jul 30 2020
@@ -76,7 +76,7 @@
               : {};
   var Konva = {
       _global: glob,
-      version: '7.0.3',
+      version: '7.0.4',
       isBrowser: detectBrowser(),
       isUnminified: /param/.test(function (param) { }.toString()),
       dblClickWindow: 400,
