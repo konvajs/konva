ad1bdefe85b1411f310c8b9e52aaa258c4eff565
Anton Lavrenov <lavrton@gmail.com>
2021-10-06 09:19:38 -0500

    build for 8.2.2

--- konva.js
+++ konva.js
@@ -5,7 +5,7 @@
 })(this, (function () { 'use strict';
 
   /*
-   * Konva JavaScript Framework v8.2.1
+   * Konva JavaScript Framework v8.2.2
    * http://konvajs.org/
    * Licensed under the MIT
    * Date: Wed Oct 06 2021
@@ -35,7 +35,7 @@
               : {};
   const Konva$2 = {
       _global: glob,
-      version: '8.2.1',
+      version: '8.2.2',
       isBrowser: detectBrowser(),
       isUnminified: /param/.test(function (param) { }.toString()),
       dblClickWindow: 400,
